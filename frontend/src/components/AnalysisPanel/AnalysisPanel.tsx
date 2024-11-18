70f6c8d3af13bc6bcf6063227316ccd338f92cbc
JmercadoASG <joshuamercadorivera@yahoo.com>
2024-11-18 14:10:10 -0400

    Change text to spanish

--- frontend/src/components/AnalysisPanel/AnalysisPanel.tsx
+++ frontend/src/components/AnalysisPanel/AnalysisPanel.tsx
@@ -120,7 +120,7 @@ export const AnalysisPanel = ({ answer, activeTab, activeCitation, citationHeigh
             >
                 <PivotItem
                     itemKey={AnalysisPanelTabs.Items}
-                    headerText="Items"
+                    headerText="Itéms"
                     // headerButtonProps={dataPoints.length === 0 ? { disabled: false,  style: { color: "grey" } } : undefined}
                     headerButtonProps={
                         dataPoints.length === 0
@@ -180,14 +180,14 @@ export const AnalysisPanel = ({ answer, activeTab, activeCitation, citationHeigh
                                 </>
                             ))
                         ) : (
-                            <p>No items available.</p>
+                            <p>No hay itéms disponibles.</p>
                         )}
                     </div>
                 </PivotItem>
 
                 <PivotItem
                     itemKey={AnalysisPanelTabs.CitationTab}
-                    headerText="Citation"
+                    headerText="Citación"
                     // headerButtonProps={isDisabledCitationTab ? pivotItemDisabledStyle : undefined}
                     headerButtonProps={
                         isDisabledCitationTab
@@ -201,7 +201,7 @@ export const AnalysisPanel = ({ answer, activeTab, activeCitation, citationHeigh
                                 {extractFilename(activeCitation)}
                             </button>
                         ) : (
-                            <p>No citation available</p>
+                            <p>No hay citaciones disponibles</p>
                         )}
                     </div>
                 </PivotItem>
